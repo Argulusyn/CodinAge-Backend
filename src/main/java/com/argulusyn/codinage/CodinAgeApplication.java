d06750d66e9e86087ccae8cc7e64e7082cd45beb
Rostislav Kozar <argulusyn@gmail.com>
2020-05-09 11:30:43 +0300

    Initial project setup

---
+++ src/main/java/com/argulusyn/codinage/CodinAgeApplication.java
@@ -0,0 +1,13 @@
+package com.argulusyn.codinage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodinAgeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodinAgeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/argulusyn/codinage/CodinAgeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.argulusyn.codinage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodinAgeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
